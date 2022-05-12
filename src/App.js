4385f05680f603049e2452f57f3a3653d3c2f25f
jadefred <97032726+jadefred@users.noreply.github.com>
2022-05-12 12:57:38 +0200

    refactoring code

--- src/App.js
+++ src/App.js
@@ -1,94 +1,108 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+const bankOne = [
+  {
+    keyCode: 81,
+    keyTrigger: "Q",
+    id: "Heater-1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    keyCode: 87,
+    keyTrigger: "W",
+    id: "Heater-2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+  {
+    keyCode: 69,
+    keyTrigger: "E",
+    id: "Heater-3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+  {
+    keyCode: 65,
+    keyTrigger: "A",
+    id: "Heater-4",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  {
+    keyCode: 83,
+    keyTrigger: "S",
+    id: "Clap",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    keyCode: 68,
+    keyTrigger: "D",
+    id: "Open-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
+  {
+    keyCode: 90,
+    keyTrigger: "Z",
+    id: "Kick-n'-Hat",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+  {
+    keyCode: 88,
+    keyTrigger: "X",
+    id: "Kick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  {
+    keyCode: 67,
+    keyTrigger: "C",
+    id: "Closed-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  },
+];
 
 function App() {
-  useEffect(() => {
-    document.addEventListener("keydown", (event) => {
-      playSound(event.key.toUpperCase());
-    });
-  }, []);
-
-  const bankOne = [
-    {
-      keyCode: 81,
-      keyTrigger: "Q",
-      id: "Heater-1",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-    },
-    {
-      keyCode: 87,
-      keyTrigger: "W",
-      id: "Heater-2",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
-    },
-    {
-      keyCode: 69,
-      keyTrigger: "E",
-      id: "Heater-3",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
-    },
-    {
-      keyCode: 65,
-      keyTrigger: "A",
-      id: "Heater-4",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
-    },
-    {
-      keyCode: 83,
-      keyTrigger: "S",
-      id: "Clap",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
-    },
-    {
-      keyCode: 68,
-      keyTrigger: "D",
-      id: "Open-HH",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
-    },
-    {
-      keyCode: 90,
-      keyTrigger: "Z",
-      id: "Kick-n'-Hat",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
-    },
-    {
-      keyCode: 88,
-      keyTrigger: "X",
-      id: "Kick",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
-    },
-    {
-      keyCode: 67,
-      keyTrigger: "C",
-      id: "Closed-HH",
-      url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
-    },
-  ];
-
-  function playSound(id) {
-    const audio = document.getElementById(id);
-    audio.play();
-  }
+  const [keyDisplay, setKeyDisplay] = useState(" ");
 
   return (
     <>
       <div id="drum-machine">
-        <div id="display"></div>
-        {bankOne.map((drumpad) => {
-          return (
-            <button
-              key={drumpad.keyCode}
-              onClick={() => {
-                playSound(drumpad.keyTrigger);
-              }}
-              className="drum-pad"
-              id={"btn-" + drumpad.keyTrigger}
-            >
-              {drumpad.keyTrigger}
-              <audio className="clip" id={drumpad.keyTrigger} src={drumpad.url}></audio>
-            </button>
-          );
-        })}
+        <div id="display">{keyDisplay}</div>
+        <div className="all-drum-keys">
+          <Drumpads setKeyDisplay={setKeyDisplay} />
+        </div>
       </div>
+    </>
+  );
+}
+
+function Drumpads(props) {
+  useEffect(() => {
+    document.addEventListener("keydown", (event) => {
+      playSound(event.key.toUpperCase());
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function playSound(keynote, id) {
+    const audio = document.getElementById(keynote);
+    audio.play();
+    props.setKeyDisplay(id);
+  }
+
+  return (
+    <>
+      {bankOne.map((drumpad) => {
+        return (
+          <button
+            key={drumpad.id}
+            onClick={() => {
+              playSound(drumpad.keyTrigger, drumpad.id);
+            }}
+            className="drum-pad"
+            id={drumpad.id}
+          >
+            {drumpad.keyTrigger}
+            <audio className="clip" id={drumpad.keyTrigger} src={drumpad.url}></audio>
+          </button>
+        );
+      })}
     </>
   );
 }
